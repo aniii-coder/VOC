99da9987195ac639ab192108303cedaf31c64252
aniii-coder <aksingh22356@gmail.com>
2024-09-12 20:41:41 +0530

    Completed Task 1

--- VOC-1.py
+++ VOC-1.py
@@ -1,1 +1,12 @@
-print('hello')
+choice = int(input(f'1)Celsius to Fahrenhiet\n2)Fahrenheit to Celsius\nEnter what you want to convert (Enter option 1 or 2): ')
+)
+if ( choice == 1 ):
+    celsius = float(input('Enter temperature in Celsius: '))
+    answer = (celsius*(9/5)+32)
+    print(f'{celsius}°C is equal to {answer}°F')
+elif(choice == 2 ):
+    fahrenheit = float(input('Enter temperature in Fahrenheit: '))
+    answer = ((fahrenheit-32)*(5/9))
+    print(f'{fahrenheit}°F is equal to {answer}°C')
+else:
+    print('Enter the valid number ')
